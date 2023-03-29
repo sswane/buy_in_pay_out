7ee8cde906a86e7f06d7d41afdcae85da57d9676
Stephanie Swaney <swaney818@gmail.com>
2023-03-29 10:47:05 -0700

    extract individual bet & allow enter button submit

--- lib/buy_in.dart
+++ lib/buy_in.dart
@@ -13,12 +13,10 @@ class BasicBuyIn extends StatefulWidget {
 class _BasicBuyIn extends State<BasicBuyIn> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   final txtController = TextEditingController();
-  final indBetController = TextEditingController();
 
   @override
   void dispose() {
     txtController.dispose();
-    indBetController.dispose();
     super.dispose();
   }
 
@@ -38,29 +36,30 @@ class _BasicBuyIn extends State<BasicBuyIn> {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 TextFormField(
-                    autofocus: true,
-                    keyboardType: TextInputType.number,
-                    controller: txtController,
-                    decoration: const InputDecoration(
-                      hintText: 'Enter buy in for all players',
+                  autofocus: true,
+                  keyboardType: TextInputType.number,
+                  controller: txtController,
+                  decoration: const InputDecoration(
+                    hintText: 'Enter buy in for all players',
+                  ),
+                  inputFormatters: [
+                    FilteringTextInputFormatter.allow(
+                      RegExp(r'^\d+\.?\d{0,2}'),
                     ),
-                    inputFormatters: [
-                      FilteringTextInputFormatter.allow(
-                        RegExp(r'^\d+\.?\d{0,2}'),
-                      ),
-                    ],
-                    validator: (String? value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Input valid buy in. Numbers up to 2 decimals allowed.';
-                      }
-                      return null;
-                    },
-                    onFieldSubmitted: (String? value) {
-                      if (_formKey.currentState!.validate()) {
-                        appState.buyInAll(double.parse(value!));
-                        txtController.clear();
-                      }
-                    }),
+                  ],
+                  validator: (String? value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Input valid buy in. Numbers up to 2 decimals allowed.';
+                    }
+                    return null;
+                  },
+                  onFieldSubmitted: (String? value) {
+                    if (_formKey.currentState!.validate()) {
+                      appState.buyInAll(double.parse(value!));
+                      txtController.clear();
+                    }
+                  },
+                ),
                 Padding(
                   padding: const EdgeInsets.symmetric(vertical: 16.0),
                   child: ElevatedButton(
@@ -81,67 +80,7 @@ class _BasicBuyIn extends State<BasicBuyIn> {
             child: Text('Buy In Per Player:'),
           ),
           for (var player in appState.playersBets)
-            ListTile(
-              subtitle: Text("\$${player.bet.toStringAsFixed(2)}"),
-              title: Text(player.name),
-              leading: IconButton(
-                icon: const Icon(
-                  Icons.edit_outlined,
-                  semanticLabel: 'Edit Buy In',
-                ),
-                color: theme.colorScheme.primary,
-                onPressed: () {
-                  showDialog(
-                    context: context,
-                    builder: (context) {
-                      return AlertDialog(
-                        title: const Text('Edit individual bet'),
-                        content: Row(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            children: [
-                              const Flexible(
-                                child: Icon(
-                                  Icons.attach_money_outlined,
-                                  semanticLabel: 'Buy In',
-                                ),
-                              ),
-                              Expanded(
-                                child: TextField(
-                                  autofocus: true,
-                                  controller: indBetController,
-                                  inputFormatters: [
-                                    FilteringTextInputFormatter.allow(
-                                      RegExp(r'^\d+\.?\d{0,2}'),
-                                    ),
-                                  ],
-                                ),
-                              ),
-                            ]),
-                        actionsAlignment: MainAxisAlignment.center,
-                        actions: <Widget>[
-                          ElevatedButton(
-                            onPressed: () {
-                              appState.editPlayerBet(
-                                  player, double.parse(indBetController.text));
-                              indBetController.clear();
-                              Navigator.pop(context);
-                            },
-                            child: const Text('Submit'),
-                          ),
-                          ElevatedButton(
-                            onPressed: () {
-                              indBetController.clear();
-                              Navigator.pop(context);
-                            },
-                            child: const Text('Cancel'),
-                          )
-                        ],
-                      );
-                    },
-                  );
-                },
-              ),
-            ),
+            IndividualBuyIn(player: player),
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 16.0),
             child: ElevatedButton(
@@ -159,6 +98,110 @@ class _BasicBuyIn extends State<BasicBuyIn> {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class IndividualBuyIn extends StatefulWidget {
+  const IndividualBuyIn({super.key, required this.player});
+  final PlayerBet player;
+  @override
+  State<IndividualBuyIn> createState() => _IndividualBuyInState();
+}
+
+class _IndividualBuyInState extends State<IndividualBuyIn> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final txtController = TextEditingController();
+
+  @override
+  void dispose() {
+    txtController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var appState = context.watch<MyAppState>();
+    var theme = Theme.of(context);
+    var player = widget.player;
+
+    return ListTile(
+      subtitle: Text("\$${player.bet.toStringAsFixed(2)}"),
+      title: Text(player.name),
+      leading: IconButton(
+        icon: const Icon(
+          Icons.edit_outlined,
+          semanticLabel: 'Edit Buy In',
+        ),
+        color: theme.colorScheme.primary,
+        onPressed: () {
+          showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                title: const Text('Edit individual bet'),
+                content: Form(
+                  key: _formKey,
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        const Flexible(
+                          child: Icon(
+                            Icons.attach_money_outlined,
+                            semanticLabel: 'Buy In',
+                          ),
+                        ),
+                        Expanded(
+                          child: TextFormField(
+                            autofocus: true,
+                            controller: txtController,
+                            inputFormatters: [
+                              FilteringTextInputFormatter.allow(
+                                RegExp(r'^\d+\.?\d{0,2}'),
+                              ),
+                            ],
+                            validator: (String? value) {
+                              if (value == null || value.isEmpty) {
+                                return 'Input valid buy in. Numbers up to 2 decimals allowed.';
+                              }
+                              return null;
+                            },
+                            onFieldSubmitted: (String? value) {
+                              if (_formKey.currentState!.validate()) {
+                                appState.editPlayerBet(
+                                    player, double.parse(txtController.text));
+                                txtController.clear();
+                                Navigator.pop(context);
+                              }
+                            },
+                          ),
+                        ),
+                      ]),
+                ),
+                actionsAlignment: MainAxisAlignment.center,
+                actions: <Widget>[
+                  ElevatedButton(
+                    onPressed: () {
+                      appState.editPlayerBet(
+                          player, double.parse(txtController.text));
+                      txtController.clear();
+                      Navigator.pop(context);
+                    },
+                    child: const Text('Submit'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      txtController.clear();
+                      Navigator.pop(context);
+                    },
+                    child: const Text('Cancel'),
+                  )
+                ],
+              );
+            },
+          );
+        },
       ),
     );
   }
