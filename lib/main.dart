fa6e526b6e289e1e5c7007630134fb17e6e9eb37
Stephanie Swaney <swaney818@gmail.com>
2023-04-11 15:10:17 -0700

    fix logic when calculating transactions; refactor/rename

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:collection/collection.dart';
@@ -129,8 +130,7 @@ class MyAppState extends ChangeNotifier {
     notifyListeners();
   }
 
-  // likely remove this
-  void findBrokeEven() {
+  void printBrokeEven() {
     for (var player in players) {
       if (player.payout == 0 && player.transactions.isEmpty) {
         debugPrint('${player.name} broke even');
@@ -138,64 +138,48 @@ class MyAppState extends ChangeNotifier {
     }
   }
 
-  // players a & b can exchange exact amounts
-  void direct(Player a, Player b) {
-    a.transactions.add(Transaction(player: b, num: a.payout));
-    b.transactions.add(Transaction(player: a, num: b.payout));
-    a.setPayout(0);
-    b.setPayout(0);
-    debugPrint('${a.name} directly matches ${b.name}');
+  void splitPlayers() {
+    winners = players.where((p) => p.payout > 0).toList();
+    losers = players.where((p) => p.payout < 0).toList();
+    debugPrint('winners: ${winners.length}');
+    debugPrint('losers: ${losers.length}');
   }
 
-  // player a is paid by player b
-  void oneWay(Player a, Player b) {
-    a.transactions.add(Transaction(player: b, num: b.payout.abs()));
-    b.transactions.add(Transaction(player: a, num: b.payout));
-    a.setPayout(a.payout + b.payout);
-    b.setPayout(0);
-    debugPrint('${a.name} can be fully paid by ${b.name}');
-  }
-
-  // player a pays player b
-  void otherWay(Player a, Player b) {
-    a.transactions.add(Transaction(player: b, num: -b.payout));
-    b.transactions.add(Transaction(player: a, num: b.payout));
-    a.setPayout(0);
-    b.setPayout(b.payout - b.payout);
-    debugPrint('${a.name} can fully pay ${b.name}');
-  }
-
-  // player a is paid by player b when remaining
-  void special(Player a, Player b) {
+  // call when player a is paid out all owed
+  void fullPayment(Player a, Player b) {
+    debugPrint(
+        '${b.name} pays \$${a.payout.toStringAsFixed(2)} to ${a.name} which was all owed');
     a.transactions.add(Transaction(player: b, num: a.payout));
     b.transactions.add(Transaction(player: a, num: -a.payout));
     b.setPayout(b.payout + a.payout);
     a.setPayout(0);
-    debugPrint('${a.name} has now been fully paid out, last by ${b.name}');
   }
 
-  void splitPlayers() {
-    winners = players.where((p) => p.payout > 0).toList();
-    losers = players.where((p) => p.payout < 0).toList();
-    debugPrint('total winners: ${winners.length}');
-    debugPrint('total losers: ${losers.length}');
+  // player a is paid by player b but is still owed money
+  void partialPayment(Player a, Player b) {
+    debugPrint(
+        '${b.name} owes \$${b.payout.abs().toStringAsFixed(2)} and pays all to ${a.name}');
+    a.transactions.add(Transaction(player: b, num: b.payout.abs()));
+    b.transactions.add(Transaction(player: a, num: b.payout));
+    a.setPayout(a.payout + b.payout);
+    b.setPayout(0);
   }
 
   void calculateTransactions(List<Player> players) {
     debugPrint('total players: ${players.length}');
-
     // Find players who broke even, should not pay or be paid
-    findBrokeEven();
+    if (kDebugMode) {
+      printBrokeEven();
+    }
     // Split into winners & losers
     splitPlayers();
 
     // Find players where one owes the exact amount one is due
-    for (var i = 0; i < winners.length; i++) {
-      debugPrint('winner looking for direct: ${winners[i].name}');
-      var match =
-          losers.firstWhereOrNull((p) => p.payout + winners[i].payout == 0);
+    for (var winner in winners) {
+      debugPrint('winner looking for direct: ${winner.name}');
+      var match = losers.firstWhereOrNull((p) => p.payout + winner.payout == 0);
       if (match != null) {
-        direct(winners[i], match);
+        fullPayment(winner, match);
         losers.remove(match);
       }
     }
@@ -207,65 +191,29 @@ class MyAppState extends ChangeNotifier {
     winners.sort((p1, p2) => p2.payout.compareTo(p1.payout));
     losers.sort((p1, p2) => p2.payout.compareTo(p1.payout));
 
-    // lowest loser could directly pay the highest winner
-    if (losers.isNotEmpty &&
-        winners.isNotEmpty &&
-        winners.first.payout + losers.first.payout >= 0) {
-      for (var i = 0; i < winners.length; i++) {
-        debugPrint('winner before one way: ${winners[i].name}');
-        while (winners[i].payout > 0) {
+    while (losers.isNotEmpty && winners.isNotEmpty) {
+      for (var winner in winners) {
+        debugPrint('winner looking for payment: ${winner.name}');
+        while (winner.payout > 0) {
+          if (losers.length == 1) {
+            fullPayment(winner, losers.first);
+            losers.removeWhere((p) => p.payout == 0);
+            break;
+          }
           var match =
-              losers.lastWhereOrNull((p) => p.payout + winners[i].payout >= 0);
+              losers.lastWhereOrNull((p) => p.payout + winner.payout >= 0);
           if (match != null) {
-            oneWay(winners[i], match);
+            partialPayment(winner, match);
             losers.remove(match);
           } else {
-            debugPrint('${winners[i].name} needs more money');
-            if (losers.length == 1) {
-              special(winners[i], losers.first);
-              losers.removeWhere((p) => p.payout == 0);
-            }
+            debugPrint('${winner.name} needs more money');
             break;
           }
         }
       }
       winners.removeWhere((p) => p.payout == 0);
-      debugPrint('winners after one way: ${winners.length}');
-      debugPrint('losers after one way: ${losers.length}');
+      debugPrint('winners: ${winners.length}');
+      debugPrint('losers: ${losers.length}');
     }
-
-    // Is the rest of this even needed??? Is there more needed after?
-
-    //   losers.sort((p1, p2) => p1.payout.compareTo(p2.payout));
-
-    //   if (losers.isNotEmpty &&
-    //       winners.isNotEmpty &&
-    //       losers.last.payout + winners.last.payout <= 0) {
-    //     // biggest loser could directly pay lowest winner
-    //     for (var i = 0; i < losers.length; i++) {
-    //       debugPrint('loser before other way: ${losers[i].name}');
-    //       debugPrint(losers[i].payout.toStringAsFixed(2));
-    //       while (losers[i].payout < 0) {
-    //         debugPrint(losers[i].payout.toStringAsFixed(2));
-    //         var match =
-    //             winners.firstWhereOrNull((p) => p.payout + losers[i].payout <= 0);
-    //         if (match != null) {
-    //           otherWay(losers[i], match);
-    //           winners.remove(match);
-    //         } else {
-    //           debugPrint('${losers[i].name} needs to pay out more money');
-    //           if (winners.length == 1) {
-    //             special(losers[i], winners.first);
-    //             winners.removeWhere((p) => p.payout == 0);
-    //           }
-    //           break;
-    //         }
-    //       }
-    //     }
-    //     losers.removeWhere((p) => p.payout == 0);
-
-    //     debugPrint('winners after other way: ${winners.length}');
-    //     debugPrint('losers after other way: ${losers.length}');
-    //   }
   }
 }
